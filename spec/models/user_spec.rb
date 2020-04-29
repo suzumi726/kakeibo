687847401de85edcc03ce84166d7d264a9167dc7
suzumi <otsuka.suzumi@gmail.com>
2020-04-29 17:01:51 +0900

    テストコード

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+describe User do
+  describe '#create' do
+    it "is invalid without a nickname" do
+      user = User.new(nickname: "", email: "kkk@gmail.com", password: "0000000", password_confirmation: "0000000")
+      user.valid?
+      expect(user.errors[:nickname]).to include("can't be blank")
+    end
+    
+    it "is invalid without an email" do
+      user = User.new(nickname: "otsuka", email: "", password: "0000000", password_confirmation: "0000000" )
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+  end
+end
